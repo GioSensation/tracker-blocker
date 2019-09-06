77e1e5ec3b677a14d780aceaeaf7ea02c9b8fb8f
Emanuele Feliziani <feliziani.emanuele@gmail.com>
2019-09-06 16:22:41 +0200

    Filter trackers only on third-party pages
    Allows Google, Facebook, Twitter, etc to work as expected.
    
    Signed-off-by: Emanuele Feliziani <feliziani.emanuele@gmail.com>

--- src/bg/background.js
+++ src/bg/background.js
@@ -1,7 +1,27 @@
 import {trackerArray, trackerSet} from '../static/data';
+import {getDomain} from 'tldjs';
+
+/**
+ * Given a url, returns a string formatted as our tracker array
+ * @param url
+ * @returns {string}
+ * @ref https://developer.chrome.com/extensions/match_patterns
+ */
+const parseUrl = url => {
+  // we extract the hostname to increase tldjs' performance: https://github.com/oncletom/tld.js#performances
+  const {hostname} = new URL(url);
+  return `*://*.${getDomain(hostname)}/*`;
+};
 
 chrome.webRequest.onBeforeRequest.addListener(
-  (details) => ({cancel: true}),
+  (details) => {
+    // Do not block if we are on a tracker's own page, such as Google or Facebook
+    // details.initiator is the origin of the request
+    if (details.initiator && !trackerSet.has(parseUrl(details.initiator))) {
+      return {cancel: true};
+    }
+    return {};
+  },
   {urls: trackerArray},
   ['blocking']
 );
