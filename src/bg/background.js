402c61a1972e2f8c6c6ae6eae95d9a7c6ed902ca
Emanuele Feliziani <feliziani.emanuele@gmail.com>
2019-09-06 15:22:02 +0200

    Externalise the data set for the trackers
    Now that we use a bundler, this makes perfect sense.
    
    Signed-off-by: Emanuele Feliziani <feliziani.emanuele@gmail.com>

--- src/bg/background.js
+++ src/bg/background.js
@@ -1,29 +1,8 @@
-const trackerList = [
-  '*://*.doubleclick.net/*',
-  '*://*.google-analytics.com/*',
-  '*://*.gstatic.com/*',
-  '*://*.google.com/*',
-  '*://*.facebook.com/*',
-  '*://*.googlesyndication.com/*',
-  '*://*.facebook.net/*',
-  '*://*.googleadservices.com/*',
-  '*://*.fonts.googleapis.com/*',
-  '*://*.scorecardresearch.com/*',
-  '*://*.adnxs.com/*',
-  '*://*.twitter.com/*',
-  '*://*.fbcdn.net/*',
-  '*://*.ajax.googleapis.com/*',
-  '*://*.yahoo.com/*',
-  '*://*.rubiconproject.com/*',
-  '*://*.openx.net/*',
-  '*://*.googletagservices.com/*',
-  '*://*.mathtag.com/*',
-  '*://*.advertising.com/*',
-];
+import {trackerArray, trackerSet} from '../static/data';
 
 chrome.webRequest.onBeforeRequest.addListener(
   (details) => ({cancel: true}),
-  {urls: trackerList},
+  {urls: trackerArray},
   ['blocking']
 );
 
