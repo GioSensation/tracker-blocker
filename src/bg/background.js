cac8d626028ff51074c19b349d820dab21e32874
Emanuele Feliziani <feliziani.emanuele@gmail.com>
2019-09-08 09:20:14 +0200

    Create a helper to update the badge
    This will be handy in a coming update.
    
    Signed-off-by: Emanuele Feliziani <feliziani.emanuele@gmail.com>

--- src/bg/background.js
+++ src/bg/background.js
@@ -14,10 +14,13 @@ chrome.webNavigation.onBeforeNavigate.addListener(({parentFrameId, tabId}) => {
   // Reset only if top frame (iFrame navigation does not reset the counter)
   if (parentFrameId < 0) {
     blockedStore.reset(tabId);
+    updateBadge(tabId);
   }
 });
 
 // Show the appropriate badge counter when switching to a new tab
-chrome.tabs.onActivated.addListener(({tabId}) => {
+chrome.tabs.onActivated.addListener(({tabId}) => updateBadge(tabId));
+
+const updateBadge = tabId => {
   chrome.browserAction.setBadgeText({text: blockedStore.getString(tabId)});
-});
+};
