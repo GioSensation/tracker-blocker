5bc6a5aeccbe31d26e34decb7522eae0f7fc51f0
Emanuele Feliziani <feliziani.emanuele@gmail.com>
2019-09-08 10:21:34 +0200

    Move the filter callback back inline
    In the end, it is better to keep the callback inline to increase the readability of the flow. The function is now simple enough to grant this.
    
    Signed-off-by: Emanuele Feliziani <feliziani.emanuele@gmail.com>

--- src/bg/background.js
+++ src/bg/background.js
@@ -1,10 +1,18 @@
-import {filterCallback} from '../core/core';
-import {trackerArray} from '../static/data';
+import {parseUrl} from '../core/core';
+import {trackerArray, trackerSet} from '../static/data';
 import blockedStore from '../core/BlockedStore';
 
 // Block requests that match our trackerArray
 chrome.webRequest.onBeforeRequest.addListener(
-  filterCallback,
+  ({initiator, tabId}) => {
+    // Do not block if we are on a tracker's own page, such as Google or Facebook
+    if (initiator && !trackerSet.has(parseUrl(initiator))) {
+      blockedStore.increase(tabId);
+      chrome.tabs.get(tabId, ({active}) => active && updateBadge(tabId));
+      return {cancel: true};
+    }
+    return {};
+  },
   {urls: trackerArray},
   ['blocking']
 );
--- src/core/core.js
+++ src/core/core.js
@@ -1,6 +1,4 @@
-import {trackerSet} from '../static/data';
 import tldjs from 'tldjs';
-import blockedStore from './BlockedStore.js';
 
 /**
  * Given a url, returns a string formatted as our tracker array
@@ -14,22 +12,4 @@ const parseUrl = url => {
   return `*://*.${tldjs.getDomain(hostname)}/*`;
 };
 
-/**
- * Callback for onBeforeNavigate to cancel requests to tracker urls
- * @param {string} details.initiator - the origin url
- * @param {number} details.tabId - the tabId where the request was initiated
- * @returns {{}|{cancel: boolean}}
- */
-const filterCallback = ({initiator, tabId}) => {
-  // Do not block if we are on a tracker's own page, such as Google or Facebook
-  if (initiator && !trackerSet.has(parseUrl(initiator))) {
-    blockedStore.increase(tabId);
-    chrome.tabs.get(tabId, ({active}) => {
-      if (active) chrome.browserAction.setBadgeText({text: blockedStore.getString(tabId)});
-    });
-    return {cancel: true};
-  }
-  return {};
-};
-
-export {filterCallback};
+export {parseUrl};
