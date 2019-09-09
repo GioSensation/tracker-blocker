d6c396c31852420edce601d296e5281aebcd2d28
Emanuele Feliziani <feliziani.emanuele@gmail.com>
2019-09-09 10:50:47 +0200

    Allow redirects to work
    This avoids breaking things such as affiliate links that routes the user through some of our trackers to attribute the link click. I am not sure whether this behaviour is acceptable or not, needs to be discussed.
    
    Signed-off-by: Emanuele Feliziani <feliziani.emanuele@gmail.com>

--- src/bg/background.js
+++ src/bg/background.js
@@ -5,9 +5,12 @@ import blockedStore from '../core/BlockedStore';
 
 // Block requests that match our trackerArray
 chrome.webRequest.onBeforeRequest.addListener(
-  ({initiator, tabId}) => {
-    // Do not block if we are on a tracker's own page, such as Google or Facebook
-    if (initiator && !trackerSet.has(parseUrl(initiator))) {
+  ({initiator, tabId, type}) => {
+    if (
+      initiator // if not set, the request might be sent from a preload or similar
+      && !trackerSet.has(parseUrl(initiator)) // do not block if we are on a tracker's own page, ie. Google, Facebook
+      && type !== 'main_frame' // allow redirects, ie. when clicking on affiliate links
+    ) {
       blockedStore.increase(tabId);
       chrome.tabs.get(tabId, ({status}) => {
         // We only update the badge here if the tab is loaded, otherwise we wait for the tab to finish loading
